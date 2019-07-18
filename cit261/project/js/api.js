c7fac15c23e4303153fbdacb3d50cb88c7fef976
Giannagp <giannagp@users.noreply.github.com>
2019-07-18 07:53:30 -0600

    up

--- cit261/project/js/api.js
+++ cit261/project/js/api.js
@@ -1,176 +1,176 @@
  
-  // let tableSlc = document.querySelector('.tableSlc'); // looking for something in out html
+  let tableSlc = document.querySelector('.tableSlc'); // looking for something in out html
   
   
-  // fetch('https://api.apixu.com/v1/forecast.json?key=3207efad383e414cabc164042190207&q=84112&days=4')
-  //   .then(
-  //     (response) => {    //could be anything not response. Here we store the promise in a variable
-  //       response.json()  //.json is a method to get the date from the promise
-  //         .then(
-  //           (weather) => {
+  fetch('https://api.apixu.com/v1/forecast.json?key=3207efad383e414cabc164042190207&q=84112&days=4')
+    .then(
+      (response) => {    //could be anything not response. Here we store the promise in a variable
+        response.json()  //.json is a method to get the date from the promise
+          .then(
+            (weather) => {
   
-  //             //output list
-  //             weather.forecast.forecastday .forEach(    //  podemos usar esto porque result es una array.
-  //               (info) => {
+              //output list
+              weather.forecast.forecastday .forEach(    //  podemos usar esto porque result es una array.
+                (info) => {
                      
-  //                   //create the row
-  //                   let rowOne = document.createElement('tr');
+                    //create the row
+                    let rowOne = document.createElement('tr');
 
-  //                   // Create the td 
-  //                   let week_days = document.createElement('td');                  
-  //                   let temp_max = document.createElement('td'); 
-  //                   let temp_min = document.createElement('td');
+                    // Create the td 
+                    let week_days = document.createElement('td');                  
+                    let temp_max = document.createElement('td'); 
+                    let temp_min = document.createElement('td');
 
-  //                   // Put the info
-  //                   week_days.textContent = info.date;
-  //                   temp_max.textContent = info.day.maxtemp_f; 
-  //                   temp_min.textContent = info.day.mintemp_f;
+                    // Put the info
+                    week_days.textContent = info.date;
+                    temp_max.textContent = info.day.maxtemp_f; 
+                    temp_min.textContent = info.day.mintemp_f;
 
 
 
-  //                 //Set the id and classes
-  //                 rowOne.setAttribute("class", "hTr");
-  //                 week_days.setAttribute("class", "hTd");
-  //                 temp_max.setAttribute("class", "hTd");
-  //                 temp_min.setAttribute("class", "hTd");
+                  //Set the id and classes
+                  rowOne.setAttribute("class", "hTr");
+                  week_days.setAttribute("class", "hTd");
+                  temp_max.setAttribute("class", "hTd");
+                  temp_min.setAttribute("class", "hTd");
 
-  //                 // add info to the row  
-  //                 rowOne.appendChild(week_days);
-  //                 rowOne.appendChild(temp_max);
-  //                 rowOne.appendChild(temp_min);
+                  // add info to the row  
+                  rowOne.appendChild(week_days);
+                  rowOne.appendChild(temp_max);
+                  rowOne.appendChild(temp_min);
 
 
   
-  //                 //add info in the table
-  //                 tableSlc.appendChild(rowOne);
+                  //add info in the table
+                  tableSlc.appendChild(rowOne);
 
-  //               }
-  //             );
+                }
+              );
   
               
   
-  // console.log(weather);
+  console.log(weather);
   
   
-  //           }
-  //         )
-  //     }
-  //   )
-  //   .catch();
+            }
+          )
+      }
+    )
+    .catch();
     
-  //   let tablePro = document.querySelector('.tablePro'); // looking for something in out html
+    let tablePro = document.querySelector('.tablePro'); // looking for something in out html
   
   
-  //   fetch('https://api.apixu.com/v1/forecast.json?key=3207efad383e414cabc164042190207&q=84601&days=4')
-  //     .then(
-  //       (response) => {    //could be anything not response. Here we store the promise in a variable
-  //         response.json()  //.json is a method to get the date from the promise
-  //           .then(
-  //             (weather) => {
+    fetch('https://api.apixu.com/v1/forecast.json?key=3207efad383e414cabc164042190207&q=84601&days=4')
+      .then(
+        (response) => {    //could be anything not response. Here we store the promise in a variable
+          response.json()  //.json is a method to get the date from the promise
+            .then(
+              (weather) => {
     
-  //               //output list
-  //               weather.forecast.forecastday .forEach(    //  podemos usar esto porque result es una array.
-  //                 (info) => {
+                //output list
+                weather.forecast.forecastday .forEach(    //  podemos usar esto porque result es una array.
+                  (info) => {
                        
-  //                     //create the row
-  //                     let rowOne = document.createElement('tr');
+                      //create the row
+                      let rowOne = document.createElement('tr');
   
-  //                     // Create the td 
-  //                     let week_days = document.createElement('td');                  
-  //                     let temp_max = document.createElement('td'); 
-  //                     let temp_min = document.createElement('td');
+                      // Create the td 
+                      let week_days = document.createElement('td');                  
+                      let temp_max = document.createElement('td'); 
+                      let temp_min = document.createElement('td');
   
-  //                     // Put the info
-  //                     week_days.textContent = info.date;
-  //                     temp_max.textContent = info.day.maxtemp_f; 
-  //                     temp_min.textContent = info.day.mintemp_f;
+                      // Put the info
+                      week_days.textContent = info.date;
+                      temp_max.textContent = info.day.maxtemp_f; 
+                      temp_min.textContent = info.day.mintemp_f;
   
   
   
-  //                   //Set the id and classes
-  //                   rowOne.setAttribute("class","hTr");
-  //                   week_days.setAttribute("class", "hTd");
-  //                   temp_max.setAttribute("class", "hTd");
-  //                   temp_min.setAttribute("class", "hTd");
+                    //Set the id and classes
+                    rowOne.setAttribute("class","hTr");
+                    week_days.setAttribute("class", "hTd");
+                    temp_max.setAttribute("class", "hTd");
+                    temp_min.setAttribute("class", "hTd");
   
-  //                   // add info to the row  
-  //                   rowOne.appendChild(week_days);
-  //                   rowOne.appendChild(temp_max);
-  //                   rowOne.appendChild(temp_min);
+                    // add info to the row  
+                    rowOne.appendChild(week_days);
+                    rowOne.appendChild(temp_max);
+                    rowOne.appendChild(temp_min);
   
   
     
-  //                   //add info in the table
-  //                   tablePro.appendChild(rowOne);
+                    //add info in the table
+                    tablePro.appendChild(rowOne);
   
-  //                 }
-  //               );
+                  }
+                );
     
                 
     
-  //   console.log(weather);
+    console.log(weather);
     
     
-  //             }
-  //           )
-  //       }
-  //     )
-  //     .catch();
-  //     let tablePci = document.querySelector('.tablePci'); // looking for something in out html
+              }
+            )
+        }
+      )
+      .catch();
+      let tablePci = document.querySelector('.tablePci'); // looking for something in out html
   
   
-  //     fetch('https://api.apixu.com/v1/forecast.json?key=3207efad383e414cabc164042190207&q=84060&days=4')
-  //       .then(
-  //         (response) => {    //could be anything not response. Here we store the promise in a variable
-  //           response.json()  //.json is a method to get the date from the promise
-  //             .then(
-  //               (weather) => {
+      fetch('https://api.apixu.com/v1/forecast.json?key=3207efad383e414cabc164042190207&q=84060&days=4')
+        .then(
+          (response) => {    //could be anything not response. Here we store the promise in a variable
+            response.json()  //.json is a method to get the date from the promise
+              .then(
+                (weather) => {
       
-  //                 //output list
-  //                 weather.forecast.forecastday .forEach(    //  podemos usar esto porque result es una array.
-  //                   (info) => {
+                  //output list
+                  weather.forecast.forecastday .forEach(    //  podemos usar esto porque result es una array.
+                    (info) => {
                          
-  //                       //create the row
-  //                       let rowOne = document.createElement('tr');
+                        //create the row
+                        let rowOne = document.createElement('tr');
     
-  //                       // Create the td 
-  //                       let week_days = document.createElement('td');                  
-  //                       let temp_max = document.createElement('td'); 
-  //                       let temp_min = document.createElement('td');
+                        // Create the td 
+                        let week_days = document.createElement('td');                  
+                        let temp_max = document.createElement('td'); 
+                        let temp_min = document.createElement('td');
     
-  //                       // Put the info
-  //                       week_days.textContent = info.date;
-  //                       temp_max.textContent = info.day.maxtemp_f; 
-  //                       temp_min.textContent = info.day.mintemp_f;
+                        // Put the info
+                        week_days.textContent = info.date;
+                        temp_max.textContent = info.day.maxtemp_f; 
+                        temp_min.textContent = info.day.mintemp_f;
     
     
     
-  //                     //Set the id and classes
-  //                     rowOne.setAttribute("class", "hTr");
-  //                     week_days.setAttribute("class", "hTd");
-  //                     temp_max.setAttribute("class", "hTd");
-  //                     temp_min.setAttribute("class", "hTd");
+                      //Set the id and classes
+                      rowOne.setAttribute("class", "hTr");
+                      week_days.setAttribute("class", "hTd");
+                      temp_max.setAttribute("class", "hTd");
+                      temp_min.setAttribute("class", "hTd");
     
-  //                     // add info to the row  
-  //                     rowOne.appendChild(week_days);
-  //                     rowOne.appendChild(temp_max);
-  //                     rowOne.appendChild(temp_min);
+                      // add info to the row  
+                      rowOne.appendChild(week_days);
+                      rowOne.appendChild(temp_max);
+                      rowOne.appendChild(temp_min);
     
     
       
-  //                     //add info in the table
-  //                     tablePci.appendChild(rowOne);
+                      //add info in the table
+                      tablePci.appendChild(rowOne);
     
-  //                   }
-  //                 );
+                    }
+                  );
       
                   
       
-  //     console.log(weather);
+      console.log(weather);
       
       
-  //               }
-  //             )
-  //         }
-  //       )
-  //       .catch();
+                }
+              )
+          }
+        )
+        .catch();
